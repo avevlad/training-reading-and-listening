3cb8d40db388bbaabe7cc0330c31b62f1de88933
avevlad <vlad.open.source@gmail.com>
2019-02-18 15:49:59 +0000

    [skip ci] Updates

---
+++ build/precache-manifest.7c2887f3b3670af68e04c98c968314fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d790ae8cc37ab647188e",
+    "url": "/static/css/main.7ba908c6.chunk.css"
+  },
+  {
+    "revision": "d790ae8cc37ab647188e",
+    "url": "/static/js/main.d790ae8c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8a7774cc885903e8966e",
+    "url": "/static/css/2.8745b01e.chunk.css"
+  },
+  {
+    "revision": "8a7774cc885903e8966e",
+    "url": "/static/js/2.8a7774cc.chunk.js"
+  },
+  {
+    "revision": "62cef4258ba45d7d1fbb2ad1238b7a69",
+    "url": "/index.html"
+  }
+];
--- utils/parse-youtube-url.ts
+++
@@ -1,5 +0,0 @@
-export default function parseYoutubeUrl(url: string): string {
-  const regexp = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=|attribution_link\?a=.+?watch.+?v(?:%|=)))((\w|-){11})(?:\S+)?$/;
-  const res = url.match(regexp);
-  return String(res![1]);
-}
